9b1461c4f17b9ebd73e228275e9f9e371e6b84e2
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-03-29 14:38:23 +0100

    Added constructors

--- Practica2/Mystr.cpp
+++ Practica2/Mystr.cpp
@@ -3,18 +3,28 @@
 Mystr::Mystr()
 {
 	this->capacity = 0;
-	this->string = (char *) malloc(sizeof(char));
+	this->string = (char *) malloc(sizeof(char));//Allocates space for '\0'
 	this->string[0] = '\0';
 }
 
 
 Mystr::Mystr(const Mystr &other)
 {
+	this->capacity = other.capacity;
+	this->string = (char *) malloc(sizeof(char) * (this->capacity + 1));//Space for string + '\0'
+	assert(this->string != NULL);//Space was allocated
 
+	strcpy(this->string, other.string);//Copies string
 }
 
 
 Mystr::Mystr(const char *other)
 {
+	assert(other != nullptr);//String exists
 
+	this->capacity = strlen(other);//Capaity is length of string to copy
+	this->string = (char *)malloc(sizeof(char) * (this->capacity + 1));//Space for string + '\0'
+	assert(this->string != NULL);//Space was allocated
+
+	strcpy(this->string, other);//Copies string
 }
--- Practica2/Mystr.h
+++ Practica2/Mystr.h
@@ -4,17 +4,36 @@
 #include <cstdlib>
 #include <cstring>
 
-#define MEM_MULTIPLIER 2//Cuánto espacio de más se debe reservar para string (Multiplicador)
-#define MEM_ADDER 0//Cuánto espacios de más se deben reservar para string (Sumador)
+#define MEM_MULTIPLIER 2//How much extra space shall be allocated (multiplier)
+#define MEM_ADDER 0//How much extra space shall be allocated (adder)
 
 class Mystr
 {
 	int capacity;
 	char *string;
 public:
+	/**************/
+	/*CONSTRUCTORS*/
+	/**************/
+	//Creates string with '\0' as the only character
 	Mystr();
+	
+	//Copies another str object
 	Mystr(const Mystr &other);
+	
+	//Creates a Mystr object from the given string
+	//@Precond		other != NULL
 	Mystr(const char *other);
+
+
+	/***********/
+	/*FUNCTIONS*/
+	/***********/
+
+
+	/************/
+	/*DESTRUCTOR*/
+	/************/
 	~Mystr();
 };
 
