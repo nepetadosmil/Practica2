0f0dc9a585f87b74632633400df7baf84f73ec45
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-03-29 13:58:36 +0100

    Test

--- Practica2/Mystr.cpp
+++ Practica2/Mystr.cpp
@@ -1,9 +1,23 @@
 #include "Mystr.h"
 
 
-
 Mystr::Mystr()
 {
+	this->capacity = 0;
+	this->string = (char *) malloc(sizeof(char));
+	this->string[0] = '\0';
+}
+
+
+Mystr::Mystr(const Mystr &other)
+{
+	cdkñjcnebfciubiñen
+}
+
+
+Mystr::Mystr(const char *other)
+{
+
 }
 
 
--- Practica2/Mystr.h
+++ Practica2/Mystr.h
@@ -1,12 +1,20 @@
 #pragma once
 
+#include <assert.h>
+#include <cstdlib>
+#include <cstring>
+
 #define MEM_MULTIPLIER 2//Cuánto espacio de más se debe reservar para string (Multiplicador)
 #define MEM_ADDER 0//Cuánto espacios de más se deben reservar para string (Sumador)
 
 class Mystr
 {
+	int capacity;
+	char *string;
 public:
 	Mystr();
+	Mystr(const Mystr &other);
+	Mystr(const char *other);
 	~Mystr();
 };
 
