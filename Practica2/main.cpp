8f8c5c522e4e7916873f9737321944f750c836b1
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-04-07 15:49:45 +0200

    Added first part of main testing program

--- Practica2/main.cpp
+++ Practica2/main.cpp
@@ -8,7 +8,8 @@
 #include <iostream>
 
 int main() {
-
+	/**************/
+	std::cout << "-- Create 3 main strings --" << std::endl;
 	Mystr a("Kha\'Zix");
 	std::cout << "String a: ";
 	a.Print();
@@ -20,17 +21,37 @@ int main() {
 	Mystr c("Tresh");
 	std::cout << "String c: ";
 	c.Print();
+	/**************/
+	Mystr d(a.Concatenate(Mystr("ola que Ashe")));
+	std::cout << "Copied string 'a' and concatenated 'ola que Ashe': ";
+	d.Print();
 
-	Mystr copia(a);
-	std::cout << "Copied string: ";
-	copia.Print();
+	Mystr e(d.Substring(11, d.Length() - 7));
+	std::cout << "11 characters cut from left and 7 from right: ";
+	e.Print();
 
-	std::cout << "Concatenated 'ola que Ashe': ";
-	copia.Concatenate(Mystr("ola que Ashe")).Print();
+	Mystr f(e.Concatenate(Mystr("Ketchup")));
+	std::cout << "Concatenated 'Ketchup': ";
+	f.Print();
 
-	a.Remove('a');
+	std::cout << "Made it all uppercase (" << f.ToUpper() << " characters changed): ";
+	f.Print();
+	/**************/
+	std::cout << "Made string 'a' all lowercase (" << a.ToLower() << " characters changed): ";
 	a.Print();
 
+	Mystr g(a.Concatenate(b.Concatenate(c)));
+	std::cout << "Concatenated strings 'a', 'b' and 'c': ";
+	g.Print();
+
+	if (g > f) {//Imprime primero la cadena mayor
+		g.Print();
+		f.Print();
+	}
+	else {
+		f.Print();
+		g.Print();
+	}
 
 	system("pause");
 
